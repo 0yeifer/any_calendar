ed0926a2eb6e3503ff00c657cdcd5e91b897ab98
Yeifer <developer@yeifer.co>
2025-04-12 04:24:02 +0000

    feat: Override function in google calendar to handle exceptions

--- extended_calendars/__init__.py
+++ extended_calendars/__init__.py
@@ -3,5 +3,9 @@ __version__ = "0.0.1"
 # Overrides for Standard Frappe Doctypes 
 
 from frappe.integrations.doctype.google_calendar import google_calendar
-from extended_calendars.overrides.google_calendar.custom_google_calendar import update_event_in_calendar as custom_update_event_in_calendar
+from extended_calendars.overrides.google_calendar.custom_google_calendar import (
+    update_event_in_calendar as custom_update_event_in_calendar,
+    get_google_calendar_object as custom_get_google_calendar_object,
+)
 google_calendar.update_event_in_calendar = custom_update_event_in_calendar
+google_calendar.get_google_calendar_object = custom_get_google_calendar_object
--- extended_calendars/overrides/google_calendar/custom_google_calendar.py
+++ extended_calendars/overrides/google_calendar/custom_google_calendar.py
@@ -1,5 +1,8 @@
 import frappe
-from frappe.integrations.doctype.google_calendar.google_calendar import update_event_in_calendar as original_update_event_in_calendar
+from frappe.integrations.doctype.google_calendar.google_calendar import (
+    update_event_in_calendar as original_update_event_in_calendar,
+    get_google_calendar_object as original_get_google_calendar_object,
+)
 
 def update_event_in_calendar(account, event, recurrence=None):
     event = frappe.get_doc("Event", {"google_calendar_event_id": event.get("id")})
@@ -7,3 +10,13 @@ def update_event_in_calendar(account, event, recurrence=None):
         original_update_event_in_calendar(account, event, recurrence)
     else:
         return
+    
+def get_google_calendar_object(g_calendar):
+    try:
+        google_calendar, account = original_get_google_calendar_object(g_calendar)
+    except Exception as e:
+        account = frappe._dict()
+        account.push_to_google_calendar = None
+        google_calendar = None
+        frappe.log_error(f"Error fetching Google Calendar object: {str(e)}")
+    return google_calendar, account
