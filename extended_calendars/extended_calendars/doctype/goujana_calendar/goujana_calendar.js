cfa16cd3383bf85720d1d6175d8ba6b326b56de0
Yeifer <developer@yeifer.co>
2025-06-17 19:39:10 +0000

    feat: add Goujana Calendar DocType with initial fields and structure

---
+++ extended_calendars/extended_calendars/doctype/goujana_calendar/goujana_calendar.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Yeifer and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Goujana Calendar", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ extended_calendars/extended_calendars/doctype/goujana_calendar/test_goujana_calendar.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Yeifer and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestGoujanaCalendar(FrappeTestCase):
+	pass
